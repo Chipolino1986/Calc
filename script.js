4e7829b0b33a727106f5cb1b3163240bf3fb50f4
Evgen Cheban <don.evgen86@gmail.com>
2022-08-29 14:36:44 +0300

    calc

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 "use strict";
 
+
 document.querySelector('.btn_plus').addEventListener('click', function() {
     let num1, num2, result;
     
     num1 = document.querySelector('.input1').value;
-    // num1 = parseInt(num1); // если input type = "text" то нужно приводить "строку" в число!
+    num1 = parseInt(num1); // если не назначить parseInt(приводит вводимые значения к ЧИСЛУ) то введёные 
+                           // значения будут плюсоваться как строки
+                           // например: 2+2 != 4 а будет 22
+
     num2 = document.querySelector('.input2').value;
-    // num2 = parseInt(num2);
+    num2 = parseInt(num2);
 
     result = num1 + num2;
     document.querySelector('.out').textContent = result; 
@@ -15,9 +19,9 @@ document.querySelector('.btn_minus').addEventListener('click', function() {
     let num1, num2, result;
 
     num1 = document.querySelector('.input1').value;
-    // num1 = parseInt(num1);
+    num1 = parseInt(num1);
     num2 = document.querySelector('.input2').value;
-    // num2 = parseInt(num2);
+    num2 = parseInt(num2);
 
     result = num1 - num2;
     document.querySelector('.out').textContent = result; 
@@ -26,9 +30,9 @@ document.querySelector('.btn_multiply').addEventListener('click', function() {
     let num1, num2, result;
 
     num1 = document.querySelector('.input1').value;
-    // num1 = parseInt(num1);
+    num1 = parseInt(num1);
     num2 = document.querySelector('.input2').value;
-    // num2 = parseInt(num2);
+    num2 = parseInt(num2);
 
     result = num1 * num2;
     document.querySelector('.out').textContent = result; 
@@ -37,9 +41,9 @@ document.querySelector('.btn_division').addEventListener('click', function() {
     let num1, num2, result;
 
     num1 = document.querySelector('.input1').value;
-    // num1 = parseInt(num1);
+    num1 = parseInt(num1);
     num2 = document.querySelector('.input2').value;
-    // num2 = parseInt(num2);
+    num2 = parseInt(num2);
 
     result = num1 / num2;
     document.querySelector('.out').textContent = result; 
